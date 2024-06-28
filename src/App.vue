582d4d0b2bd951f5135521f8b012ef6d668cb4f9
luigitarallo <tarallo.gigi@gmail.com>
2024-06-28 18:51:45 +0200

    feat: add hybrid scroll

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,68 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, onUnmounted, ref } from "vue";
 
+// Riferimenti agli elementi DOM
+const stickyWrap = ref<HTMLElement | null>(null);
+
+// Funzione per la trasformazione
+function transform(section: HTMLElement) {
+  const offsetTop = section.parentElement?.offsetTop || 0;
+  const scrollSection = section.querySelector(
+    ".horizontal_scroll"
+  ) as HTMLElement;
+
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+  percentage = percentage < 0 ? 0 : percentage > 100 ? 100 : percentage;
+
+  scrollSection.style.transform = `translate3d(${-percentage}vw, 0, 0)`;
+}
+
+// Aggiungi l'event listener allo scroll quando il componente è montato
+onMounted(() => {
+  const section = stickyWrap.value;
+  if (section) {
+    const onScroll = () => transform(section);
+    window.addEventListener("scroll", onScroll);
+
+    // Rimuovi l'event listener quando il componente è smontato
+    onUnmounted(() => {
+      window.removeEventListener("scroll", onScroll);
+    });
+  }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <!-- <section>
+      <div class="container">
+        <h1>Hybrid Scrolling</h1>
+        <p>Dummy text dummy text dummy text dummy text dummy text.</p>
+      </div>
+    </section> -->
+
+    <div class="scroll_container">
+      <div ref="stickyWrap" class="sticky_wrap">
+        <div class="horizontal_scroll">
+          <div class="scroll_contents red">
+            <h2 class="left">Hello</h2>
+          </div>
+          <!-- <div class="scroll_contents yellow"></div>
+          <div class="scroll_contents green"></div> -->
+          <div class="scroll_contents blue">
+            <h2 class="right">Goodbye</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section>
+      <div class="container">
+        <h1>The End</h1>
+        <p>Dummy text dummy text dummy text dummy text dummy text.</p>
+      </div>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
