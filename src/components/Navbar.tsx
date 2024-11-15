f37904fca378610d8bce0f75622549a0e073523d
luigitarallo <tarallo.gigi@gmail.com>
2024-11-15 10:29:41 +0100

    add link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 export function Navbar() {
   return (
-    <header>
-      <nav></nav>
-    </header>
+    <nav>
+      <ol>
+        <li>
+          <a href="">home</a>
+        </li>
+        <li>
+          <a href="">about</a>
+        </li>
+        <li>
+          <a href="">works</a>
+        </li>
+      </ol>
+    </nav>
   );
 }
