50c2d7d6254f69a1fb39f1bf51d17ff0583a1a9c
luigitarallo <tarallo.gigi@gmail.com>
2024-11-18 10:44:00 +0100

    add hardcoded description

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,15 @@
 export function Hero() {
   return (
     <section className="hero">
-      <div>
-        <p>I'am the hero</p>
+      <div className="container">
+        <h1>Hi there.</h1>
+        <p>
+          I’m<span> Luigi Tarallo </span> a
+          <span> Full Stack Web Developer </span>
+          passionate about<span> technology </span>
+          and focused on building modern, efficient
+          <span> web applications</span>.
+        </p>
       </div>
     </section>
   );
