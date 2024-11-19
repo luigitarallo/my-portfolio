c26cc0b4b6d22820abd4d3940259383b4f58d24d
luigitarallo <tarallo.gigi@gmail.com>
2024-11-19 15:34:12 +0100

    add div for about-tex

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,15 +4,16 @@ export function AboutMe() {
   return (
     <section id="about">
       <div className="container">
-        <div>
+        <div className="about-text">
           <h2>About me</h2>
           <p>
             Born and raised in the province of Salerno, a few years after
             getting my high school diploma, I moved to Naples to pursue a
-            university degree in <span>Biotechnology</span>. Between studying
-            biology and enjoying a pizza "a portafoglio," I continued to nurture
-            my earliest passions: computers and technology in all its forms. I
-            developed a deep interest in the <span>Open Source</span>{" "}
+            university degree in <span>Biotechnology</span>. <br />
+            Between studying biology and enjoying a pizza "a portafoglio," I
+            continued to nurture my earliest passions: computers and technology
+            in all its forms. I developed a deep interest in the
+            <span>Open Source</span>
             philosophy, using
             <span> Linux-based operating systems</span> and following the
             evolution of new hardware and software technologies. In my spare
@@ -21,36 +22,40 @@ export function AboutMe() {
           </p>
           <p>
             Thanks to my extensive knowledge of the tech world, in
-            <span>2018</span> I was noticed and hired by a
-            <span>digital marketing</span> company specializing in the promotion
-            and sale of <span>tech products</span>. This experience allowed me
-            to achieve important professional and personal milestones, and it
-            drove me to further explore the world of IT, not just by selling
-            technology but by actively contributing to its development.
+            <span> 2018</span> I was noticed and hired by a
+            <span> digital marketing</span> company specializing in the
+            promotion and sale of <span>tech products</span>. This experience
+            allowed me to achieve important professional and personal
+            milestones, and it drove me to further explore the world of IT, not
+            just by selling technology but by actively contributing to its
+            development.
           </p>
           <p>
-            So, in 2023, I decided to return to my hometown and pursue my
-            passion with determination, enrolling in an intensive bootcamp to
-            become a Full Stack Web Developer. During the course, I deepened my
-            knowledge of both Front-End and Back-End development, thanks to a
+            So, in <span>2023</span>, I decided to return to my hometown and
+            pursue my passion with determination, enrolling in an intensive
+            <span> bootcamp</span> to become a <span>Web Developer</span>.
+            During the course, I deepened my knowledge of both
+            <span> Front-End and Back-End development</span>, thanks to a
             structured program and guidance from industry professionals. In
             fact, the end of the bootcamp was only the beginning of a continuous
             journey of study, learning, and constant practice. I participated in
-            a selective course focused on Microsoft technologies such as C# and
-            .NET, and every day I dedicate myself to solidifying my knowledge,
-            learning new technologies, and staying up-to-date with the latest
-            trends in the field.
+            a selective course focused on<span> Microsoft technologies </span>
+            such as <span>C#</span> and
+            <span> .NET</span>, and every day I dedicate myself to solidifying
+            my knowledge, learning new technologies, and staying up-to-date with
+            the latest trends in the field.
           </p>
           <p>
             At the same time, I have collaborated with startups to develop web
             apps and continue to work on personal projects, applying and
-            refining the skills I’ve acquired. My motto is: "A day without
-            learning something new is a day wasted".
+            refining the skills I’ve acquired. My motto is:
+            <span>"A day without learning something new is a day wasted"</span>.
           </p>
           <p>
-            In my free time, I recharge by diving into books of various genres,
-            engaging in DIY projects, and playing chess and video games. I love
-            traveling and spending quality time with friends, as well as
+            In my free time, I recharge by <span>diving into books</span> of
+            various genres, engaging in <span>DIY projects</span>, and playing
+            <span>chess</span> and<span> video games</span>. I love traveling
+            and spending quality time with<span> friends</span>, as well as
             trekking in nature, which helps me recharge and find new
             inspiration.
           </p>
