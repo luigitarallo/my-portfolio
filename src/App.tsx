7cc204bbffab1e3d6993d25fe9895eafe93d48d4
luigitarallo <tarallo.gigi@gmail.com>
2024-11-19 12:51:32 +0100

    import AboutMe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
+import { AboutMe } from "./components/AboutMe";
 function App() {
   return (
     <>
       <Navbar />
       <main>
         <Hero />
+        <AboutMe />
       </main>
     </>
   );
