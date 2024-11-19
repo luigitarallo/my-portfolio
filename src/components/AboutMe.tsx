8d7a073d6d0f511ea30f5852ab8d274435340e12
luigitarallo <tarallo.gigi@gmail.com>
2024-11-19 12:50:16 +0100

    create AboutMe component

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,61 @@
+import "./AboutMe.css";
+
+export function AboutMe() {
+  return (
+    <section id="about">
+      <div className="container">
+        <div>
+          <h2>About me</h2>
+          <p>
+            Born and raised in the province of Salerno, a few years after
+            getting my high school diploma, I moved to Naples to pursue a
+            university degree in <span>Biotechnology</span>. Between studying
+            biology and enjoying a pizza "a portafoglio," I continued to nurture
+            my earliest passions: computers and technology in all its forms. I
+            developed a deep interest in the <span>Open Source</span>{" "}
+            philosophy, using
+            <span> Linux-based operating systems</span> and following the
+            evolution of new hardware and software technologies. In my spare
+            time, I provided <span>IT support</span> to acquaintances, helping
+            them troubleshoot technical issues and improve their setups.
+          </p>
+          <p>
+            Thanks to my extensive knowledge of the tech world, in
+            <span>2018</span> I was noticed and hired by a
+            <span>digital marketing</span> company specializing in the promotion
+            and sale of <span>tech products</span>. This experience allowed me
+            to achieve important professional and personal milestones, and it
+            drove me to further explore the world of IT, not just by selling
+            technology but by actively contributing to its development.
+          </p>
+          <p>
+            So, in 2023, I decided to return to my hometown and pursue my
+            passion with determination, enrolling in an intensive bootcamp to
+            become a Full Stack Web Developer. During the course, I deepened my
+            knowledge of both Front-End and Back-End development, thanks to a
+            structured program and guidance from industry professionals. In
+            fact, the end of the bootcamp was only the beginning of a continuous
+            journey of study, learning, and constant practice. I participated in
+            a selective course focused on Microsoft technologies such as C# and
+            .NET, and every day I dedicate myself to solidifying my knowledge,
+            learning new technologies, and staying up-to-date with the latest
+            trends in the field.
+          </p>
+          <p>
+            At the same time, I have collaborated with startups to develop web
+            apps and continue to work on personal projects, applying and
+            refining the skills I’ve acquired. My motto is: "A day without
+            learning something new is a day wasted".
+          </p>
+          <p>
+            In my free time, I recharge by diving into books of various genres,
+            engaging in DIY projects, and playing chess and video games. I love
+            traveling and spending quality time with friends, as well as
+            trekking in nature, which helps me recharge and find new
+            inspiration.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
