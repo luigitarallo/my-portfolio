72cd9a95ac873c80a186595c9172733c7a38e28b
luigitarallo <tarallo.gigi@gmail.com>
2024-11-22 12:03:30 +0100

    Add logic to change color when on light sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 
 export function Navbar() {
+  // useState to check light section
+  const [isLightSection, setIsLightSection] = useState(false);
+
+  // useEffect to create an IntersectionObserver instance
+  // that will watch the lightSection "Works"
+  // and change the isLightSection value
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const lightSection = entries[0];
+        setIsLightSection(lightSection.isIntersecting);
+      },
+      { threshold: 0.15 }
+    );
+
+    const lightSectionElement = document.getElementById("works");
+    if (lightSectionElement) {
+      observer.observe(lightSectionElement);
+    }
+    return () => observer.disconnect();
+  }, []);
+
+  // function to go to a specific section when clicking nav links
   const scrollToSection = (id: string) => {
     const section = document.getElementById(id);
     if (section) {
@@ -11,13 +35,28 @@ export function Navbar() {
     <nav>
       <ul>
         <li>
-          <button onClick={() => scrollToSection("home")}>home</button>
+          <button
+            className={isLightSection ? "text-dark" : "text-light"}
+            onClick={() => scrollToSection("home")}
+          >
+            home
+          </button>
         </li>
         <li>
-          <button onClick={() => scrollToSection("about")}>about</button>
+          <button
+            className={isLightSection ? "text-dark" : "text-light"}
+            onClick={() => scrollToSection("about")}
+          >
+            about
+          </button>
         </li>
         <li>
-          <button onClick={() => scrollToSection("works")}>works</button>
+          <button
+            className={isLightSection ? "text-dark" : "text-light"}
+            onClick={() => scrollToSection("works")}
+          >
+            works
+          </button>
         </li>
       </ul>
     </nav>
