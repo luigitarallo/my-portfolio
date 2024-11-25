acbebac9df97c5974c85d6163676cae6772a4ecf
luigitarallo <tarallo.gigi@gmail.com>
2024-11-25 19:11:23 +0100

    Add project structure

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,17 +1,58 @@
 import "./Works.css";
+import boolfixImg from "../assets/boolflix-screen.png";
+import tictactoeImg from "../assets/tic-tac-toe-screen.png";
+import porfolioImg from "../assets/portfolio-screen.png";
 
 export function Works() {
   return (
     <section id="works" className="debug-1">
       <div className="container">
         <h2>Works</h2>
-        <div className="works-row debug-1">
-          <div className="works-col debug"></div>
-          <div className="works-col debug"></div>
-          <div className="works-col debug"></div>
-          <div className="works-col debug"></div>
-          <div className="works-col debug"></div>
-          <div className="works-col debug"></div>
+        <div className="projects-row debug-1">
+          <div className="project-col">
+            <div className="project">
+              <div className="image-container">
+                <img src={boolfixImg} alt="boolf ix preview" />
+              </div>
+              <div className="buttons">
+                <button>Demo</button>
+                <button>GitHub</button>
+              </div>
+            </div>
+          </div>
+          <div className="project-col">
+            <div className="project">
+              <div className="image-container">
+                <img src={boolfixImg} alt="boolf ix preview" />
+              </div>
+              <div className="buttons">
+                <button>Demo</button>
+                <button>GitHub</button>
+              </div>
+            </div>
+          </div>
+          <div className="project-col debug">
+            <div className="project">
+              <div className="image-container">
+                <img src={tictactoeImg} alt="boolf ix preview" />
+              </div>
+              <div className="buttons">
+                <button>Demo</button>
+                <button>GitHub</button>
+              </div>
+            </div>
+          </div>
+          <div className="project-col debug">
+            <div className="project">
+              <div className="image-container">
+                <img src={porfolioImg} alt="boolflix preview" />
+              </div>
+              <div className="buttons">
+                <button>Demo</button>
+                <button>GitHub</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
