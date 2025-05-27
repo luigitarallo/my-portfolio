d5c7c278b0deaaad5b4ab1ad1ef31e44d6f50299
luigitarallo <tarallo.gigi@gmail.com>
2025-05-27 15:12:29 +0200

    add preloader for transition during the firs load

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+const preloader = document.getElementById("preloader");
+if (preloader) {
+  requestAnimationFrame(() => {
+    preloader.classList.add("fade-out");
+    setTimeout(() => {
+      preloader.remove();
+    }, 2000);
+  });
+}
